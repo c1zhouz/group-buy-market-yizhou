8e6b78f135f42722ba2c9db7d61935864e789c7b
yizhou <381224294@qq.com>
2025-06-23 14:18:25 +0800

    feat: 第8节

--- group-buy-market-yizhou-domain/src/main/java/cn/bugstack/domain/activity/adapter/repository/IActivityRepository.java
+++ group-buy-market-yizhou-domain/src/main/java/cn/bugstack/domain/activity/adapter/repository/IActivityRepository.java
@@ -16,4 +16,9 @@ public interface IActivityRepository {
     SCSkuActivityVO querySCSkuActivityBySCGoodsId(String source, String channel, String goodsId);
 
     boolean isTagCrowdRange(String tagId, String userId);
+
+    boolean downgradeSwitch();
+
+    boolean cutRange(String userId);
+
 }
